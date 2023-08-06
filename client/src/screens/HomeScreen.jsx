9c3ea3a67f92920275cb46b43b51a83a1331302b
darry <dmilesbacay@gmail.com>
2023-08-06 12:38:14 +0800

    fetch product in backend

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
 import Product from "../components/Product";
+import axios from "axios";
 
 const HomeScreen = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const { data } = await axios.get("/api/products");
+			setProducts(data);
+		};
+		fetchProducts();
+	}, []);
 	return (
 		<React.Fragment>
 			<h1>Latest Products</h1>
 			<Row>
 				{products.map((product, index) => {
-                    const sumIndex = index + 1
+					const sumIndex = index + 1;
 					return (
 						<Col sm={12} md={6} lg={4} xl={3} key={sumIndex}>
-                            <Product product={product}/>
+							<Product product={product} />
 						</Col>
 					);
 				})}
