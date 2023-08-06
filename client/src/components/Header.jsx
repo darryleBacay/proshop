3ce586dbdf347faa6866f8656e7ac3c21686f29d
darry <dmilesbacay@gmail.com>
2023-08-06 10:26:29 +0800

    finish implementation of react router dom

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../asssets/logo.png";
 
 const Header = () => {
@@ -8,21 +9,27 @@ const Header = () => {
 		<header>
 			<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
 				<Container>
-					<Navbar.Brand href="#">
-						<img src={logo} alt="logo" />
-						Proshop
-					</Navbar.Brand>
+					<LinkContainer to="/">
+						<Navbar.Brand>
+							<img src={logo} alt="logo" />
+							Proshop
+						</Navbar.Brand>
+					</LinkContainer>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ms-auto">
-							<Nav.Link href="/cart">
-								{" "}
-								<FaShoppingCart /> Cart
-							</Nav.Link>
-							<Nav.Link href="/login">
+							<LinkContainer to="/cart">
+								<Nav.Link>
+									{" "}
+									<FaShoppingCart /> Cart
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to="/sign-in">
+							<Nav.Link>
 								{" "}
 								<FaUser /> Sign In
 							</Nav.Link>
+							</LinkContainer>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
