ff1261a4ab92a5a84a0f0899aacd696e066e8432
darry <dmilesbacay@gmail.com>
2023-08-08 14:04:20 +0800

    fetch data from database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import productRoutes from "./routes/productRoutes.js";
 
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 connectDB(); //Connect to DataBase MongoDB
 const app = express();
@@ -14,13 +14,6 @@ app.get("/", (req, res) => {
 	res.send("APi is running......");
 });
 
-app.get("/api/products", (req, res) => {
-	res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
