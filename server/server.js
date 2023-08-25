074effa944cc3f231073d3d11a8142107013edc8
darry <dmilesbacay@gmail.com>
2023-08-26 03:47:51 +0800

    finish backend login atuhentication

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 import productRoutes from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-
+import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 const port = process.env.PORT || 5000;
 
@@ -15,10 +16,14 @@ app.get("/", (req, res) => {
 	res.send("APi is running......");
 });
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/products", productRoutes);
+app.use("/api/users/", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
- 
